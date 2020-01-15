e90f4b04b027c39ff0751e58c62fc9fac4bc4b0c
James Sofios <jamessofios@gmail.com>
2020-01-15 15:37:16 -0600

    First commit

---
+++ Image.java
@@ -0,0 +1,104 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.util.*;
+import javax.imageio.*;
+import javax.imageio.stream.*;
+
+public class Image
+{
+	private int width, height;
+	private int[][] pixels;
+
+	public Image(String filename) throws Exception
+	{
+		this.read(filename);
+	}
+
+	public int getWidth()
+	{
+		return this.width;
+	}
+
+	public int getHeight()
+	{
+		return this.height;
+	}
+
+	public void read(String filename) throws Exception
+	{
+		File fileImage = new File(filename);
+
+        BufferedImage bufImage = ImageIO.read(fileImage);
+		this.width = bufImage.getWidth();
+		this.height = bufImage.getHeight();
+
+		pixels = new int[this.height][this.width];
+
+		for (int row = 0; row < this.height; row++)
+      	{
+         	for (int col = 0; col < this.width; col++)
+         	{
+         		pixels[row][col] = bufImage.getRGB(col, row);
+         	}
+        }
+
+   }
+
+   private BufferedImage createBufferedImage()
+   {
+		BufferedImage bufImage = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_RGB);
+
+      	for (int row = 0; row < this.height; row++)
+      	{
+         	for (int col = 0; col < this.width; col++)
+         	{
+         		bufImage.setRGB(col, row, this.pixels[row][col]);
+         	}
+        }
+
+      	return bufImage;
+	}
+
+	public void flipY() {
+		int[][] p = this.pixels;
+		int[][] mirrored = new int[p.length][p[0].length];
+
+		for(int i=0; i < p.length ; i++) {
+			for(int j=(p[0].length - 1); j >= 0; j--)
+          	{
+          		mirrored[i][p[0].length - (1 + j)] = (p[i][j]);
+          	}
+        }
+
+        pixels = mirrored;
+    }
+
+    public void flipX() {
+    	int[][] p = this.pixels;
+		int[][] flipped = new int[p.length][p[0].length];
+
+		for(int r = 0; r < p.length; r++) {
+			for(int c = 0; c < p[0].length; c++)
+				flipped[flipped.length - (1+r)][flipped[0].length - (1+c)] = p[r][c];
+		}
+
+		pixels = flipped;
+	}
+
+
+   public void write(String filename) throws Exception
+   {
+        File fileImage = new File(filename);
+        String ext = filename.substring(filename.indexOf('.') + 1);
+        BufferedImage bufImage = createBufferedImage();
+        ImageIO.write(bufImage, ext, fileImage);
+   }
+
+	public void draw(Graphics gc, int x, int y)
+	{
+   		BufferedImage bufImage = createBufferedImage();
+      	gc.drawImage(bufImage, x, y, null);
+   	}
+
+}
---
+++ ImageGUI.java
@@ -0,0 +1,137 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+//frame.removeall()
+//frame.remove()
+public class ImageGUI{
+
+	static String image;
+	static Image iTest;
+	static Graphics gc;
+
+	public static void setGUI() throws Exception {
+
+		JFrame frame = new JFrame("Image Viewer");
+
+		JButton bt1 = new JButton("Open");
+		JButton bt2 = new JButton("Save");
+		JButton bt3 = new JButton("Flip X");
+		JButton bt4 = new JButton("Flip Y");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     //setting close operation.
+		frame.setLayout(new FlowLayout());		//setting layout using FlowLayout object
+		frame.setSize(900, 900);				//setting size of Jframe
+
+		JPanel panel = new JPanel(new GridBagLayout());//panel in frame
+
+		GridBagConstraints con = new GridBagConstraints();
+
+		frame.add(panel);
+
+		con.gridx = 0;
+		con.gridy = 0;
+
+		panel.add(bt1, con);
+		con.gridx++;
+
+		panel.add(bt2,con);
+		con.gridx++;
+
+		panel.add(bt3,con);
+		con.gridx++;
+
+		panel.add(bt4,con);
+
+		frame.setVisible(true);
+		gc = frame.getGraphics();// needs to come after this line: frame.setVisible(true);
+
+		bt1.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				try{
+					image = openFile();
+					iTest = new Image(image);
+					iTest.draw(gc, 100, 100);
+				}
+				catch(Exception ex){
+					ex.printStackTrace();
+				}
+			}
+		});
+
+		bt2.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				saveFile();
+			}
+		});
+
+		bt3.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+
+				iTest.flipX();
+				iTest.draw(gc, 100, 100);
+
+			}
+		});
+
+		bt4.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+
+				iTest.flipY();
+				iTest.draw(gc, 100, 100);
+
+			}
+		});
+		return;
+	}
+	public static String openFile(){
+		//Only jpg png bmp wbmp gif files work due to limitations of javax.imageio.*;
+		FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+		dialog.setMode(FileDialog.LOAD);
+
+		dialog.setVisible(true);
+
+		String file = dialog.getDirectory() + dialog.getFile();
+		System.out.println("Opening: " + file);
+
+		return file;
+	}
+
+	public static void saveFile(){
+		//Only jpg png bmp wbmp gif files work due to limitations of javax.imageio.*;
+		FileDialog dialog = new FileDialog((Frame)null, "Save your file");
+		dialog.setMode(FileDialog.SAVE);
+		dialog.setVisible(true);
+
+		String file = dialog.getDirectory() + dialog.getFile();
+
+		System.out.println("Saving to: " + file);
+
+		try{
+			iTest.write(file);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+		return;
+	}
+	public static void main(String[] args) throws Exception {
+		try{
+			setGUI();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
+
+
